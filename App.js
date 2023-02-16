bffeaf48dcc6eb06b2352306b0701a219b932ff0
Ashley Hackett <114507502+AshleyHackettCode@users.noreply.github.com>
2023-02-15 23:23:47 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Todo from "./do";
+import TodoForm from "./list";
+import Navbar from "./components/navbar.js";
+
+function App(){
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'learn react',
+      isCompleted: false,
+    },
+    {
+      text: 'meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'build todo app',
+      isCompleted: false,
+    }        
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text, isCompleted:false}];
+    setTodos(newTodos);
+  }
+  const removeTodo = index => {
+    let temp = [...todos];    
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
+
+  return(
+    
+    <div className="app">
+      <Navbar/>
+      <div className="todo-list" >
+        {todos.map((todo, i) => (
+          <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='title'>
+        <h1 className='nav'>Saturday's Park Hopper To-Do List</h1>
+    </div>
+  )
+}
+
+export default Navbar
